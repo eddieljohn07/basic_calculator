73f77d48c21e25a7b8315fc2206ce8a833753b0e
eddieljohn07 <100287870+eddieljohn07@users.noreply.github.com>
2022-12-11 22:47:15 +0800

    add addition and subtraction

--- main.py
+++ main.py
@@ -5,3 +5,7 @@ def calculator():
   answer = firstNum
   operation = input("What operation would you like? (+, -, *, /, power, square root, =): ")
   while operation != "=":
+    if operation == "+":
+      answer = firstNum + float(input("Second Number: "))
+    elif operation == "-":
+      answer = firstNum - float(input("Second Number: "))
