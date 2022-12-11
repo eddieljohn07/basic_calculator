d1eff9a890685d48ab3a14e36f9ba759a5fed6ff
eddieljohn07 <100287870+eddieljohn07@users.noreply.github.com>
2022-12-11 22:41:23 +0800

    add while loop

--- main.py
+++ main.py
@@ -4,3 +4,4 @@ def calculator():
   firstNum = float(input("First Number: "))
   answer = firstNum
   operation = input("What operation would you like? (+, -, *, /, power, square root, =): ")
+  while operation != "=":
